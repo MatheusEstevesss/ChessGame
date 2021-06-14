761932d61e0f569690195fd25a00499c4f2e7d38
MatheusEstevesss <matheusesantos@gmail.com>
2021-06-14 18:25:38 -0300

    Adding the Position class

---
+++ chess-console/chess-console/Board/Position.cs
@@ -0,0 +1,19 @@
+﻿namespace chess_console.Tabuleiro
+{
+    class Position
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Position(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return $"{Linha}, {Coluna}";
+        }
+    }
+}
